05fee91bdcab30380fc5bb1753cfcae7c5da03e2
Hack Reactor Student <student@hackreactor.com>
2017-04-20 20:13:55 -0700

    Passed all tests

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,50 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var request = require('request');
 
 
+
+var sendGitHubRequestAsync = function(username) {
+  return new Promise(function(resolve, reject) {
+    request.get('https://api.github.com/users/' + username[0], function(err, res) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve([JSON.parse(res.body), username[1]]);
+      }
+    });
+  });
+};
+
+var writeResponseAsync = function(response) {
+  return new Promise(function(resolve, reject) {
+    fs.writeFile(response[1], JSON.stringify(response[0]), 'utf8', function(err, res) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response[0]);
+      }
+    });
+  });
+};
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(readFilePath, 'utf8', function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve([data.split('\n')[0], writeFilePath]);
+      }
+    });
+  })
+  .then(function(data) {
+    return sendGitHubRequestAsync(data);
+  })
+  .then(function(data) {
+    return writeResponseAsync(data);
+  });
 };
 
 // Export these functions so we can test them
--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf-8', function(err, data) {
+    // console.log('data', data);
+    if (err) {
+      callback(err);
+    }
+    var secondArg = data.split('\n')[0];
+    // console.log(secondArg);
+    callback(err, secondArg);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request.get(url, function(err, res) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
